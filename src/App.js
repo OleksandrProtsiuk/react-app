ea61ccb1bf829b4bae27ae0526e44ab4bd62206f
Oleksandr <44535554+OleksandrProtsiuk@users.noreply.github.com>
2020-03-06 17:32:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ function App() {
   return (
     <div className="App">
         <br />
-        <h3>Hello world!</h3>
+        
+        <h3>
+            This is my Band
+        </h3>
         <hr />
-        <p>
-            This is my PWA App
-        </p>
-        <svg type="circle" />
+    
+        <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F100006981617209%2Fvideos%2F2575928975983131%2F&show_text=1&width=560" width="560" height="451" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
+        
     </div>
   );
 }
